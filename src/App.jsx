810b5ddf29352b8bf7a683fdb08f1dfff1591cfd
Paul Hwang <hwpaul1007@gmail.com>
2025-07-24 15:22:59 -0700

    Project Setup: API Fetch, React Router, Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  // const [classes, setClasses] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(
+  //     "https://classes.cornell.edu/api/2.0/config/subjects.json?roster=FA25"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setClasses(data);
+  //     })
+  //     .catch((err) => {
+  //       console.error("Error fetching subjects: " + err);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  // console.log(classes);
+
+  return (
+    <>
+      <div>
+        <h1>Hi</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
