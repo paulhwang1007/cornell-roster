8274b81dce38cba8f94a4e7883eea1ff349d0c86
Paul Hwang <hwpaul1007@gmail.com>
2025-07-29 19:01:34 -0700

    Edit SubjectListPage.jsx: Display Subjects divided by Letter

--- src/pages/SubjectListPage.jsx
+++ src/pages/SubjectListPage.jsx
@@ -4,16 +4,33 @@ import { Link, useParams } from "react-router-dom";
 const SubjectListPage = () => {
   // || States
   const { semester } = useParams();
-  const [subjects, setSubjects] = useState([]);
+  const [subjectsByLetter, setSubjectsByLetter] = useState({});
 
-  // || Fetch Subjects from provided Roster Semester
+  // Initial Roster Subject Load
   useEffect(() => {
     fetch(
       `https://classes.cornell.edu/api/2.0/config/subjects.json?roster=${semester}`
     )
       .then((response) => response.json())
       .then((data) => {
-        setSubjects(data.data.subjects);
+        const subjects = data.data.subjects;
+
+        // --- Sorts Subjects from A-Z ---
+        subjects.sort((a, b) => a.value.localeCompare(b.value));
+
+        // Key: Unique firstLetter
+        // Value: Array with the Subjects that share the same firstLetter
+        const grouped = {};
+        subjects.forEach((subject) => {
+          const firstLetter = subject.value[0].toUpperCase();
+          if (!grouped[firstLetter]) {
+            grouped[firstLetter] = []; // firstLetter: []
+          }
+          grouped[firstLetter].push(subject); // firstLetter: [subject1, subject2, ...]
+        });
+
+        // Sets subjectsByLetter = grouped
+        setSubjectsByLetter(grouped);
       })
       .catch((error) => {
         console.error("Error fetching rosters: ", error);
@@ -22,14 +39,25 @@ const SubjectListPage = () => {
 
   return (
     <div>
-      <h1>Subject List Page</h1>
-      <ul>
-        {subjects.map((subject) => (
-          <li>
-            <Link to={`/${semester}/${subject.value}`}>{subject.value}</Link>
-          </li>
+      <h1>Subjects for {semester}</h1>
+
+      {Object.keys(subjectsByLetter)
+        .sort()
+        .map((letter) => (
+          <div key={letter}>
+            <h2>{letter}</h2>
+
+            <ul>
+              {subjectsByLetter[letter].map((subject) => (
+                <li key={subject.value}>
+                  <Link to={`/${semester}/${subject.value}`}>
+                    {subject.value}: {subject.descrformal}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         ))}
-      </ul>
     </div>
   );
 };
